c737a067cc95d02847bdbe9155ad68668ec1d52d
Taj <tajmarie92@gmail.com>
2023-09-18 22:59:48 -0500

    Completed  Exercise 1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.IO.Pipes;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add1(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add2(decimal c, decimal d)
+        {
+            return c + d;
+        }
+
+        public static string Add3(int a, int b, bool withDollars)
+        {
+            if (withDollars && (a + b) != 1)
+            {
+                return $" {a + b} dollars.";
+            }
+            else if (withDollars && (a + b) == 1)
+            {
+                return $" {a + b} dollar";
+            }
+            else
+            {
+                return $" {a + b} ";
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($" 2 + 5 =  {Add1(2, 5)}");
+
+            Console.WriteLine($" 5.9 + 9.7 = {Add2(5.9m, 9.7m)} ");
+
+            Console.WriteLine($" In your bank account you have: {Add3(15, 37, true)} ");
         }
     }
 }
